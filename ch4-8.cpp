9a0515c6d5aee72a91049ba51d51851522270757
yiwen, Huang <88823977+YIWEN-Huang@users.noreply.github.com>
2021-10-18 17:10:24 +0800

    20211018 update
    Add while for all program 
    Add check if input not number. will show only input number.
    Add input Q/q exit program

--- ch4-8.cpp
+++ ch4-8.cpp
@@ -13,33 +13,41 @@ struct William_Wingate {
 
 int main() {
 	William_Wingate* ps = new William_Wingate;
-	cout << "請輸入公司名稱" << endl;
-	getline(cin, ps->name);
-	cout << "請輸入PIZZA重量" << endl;
-	cin >> ps->weight;
-	while (cin.fail()) {
-		cout << "請輸入數字" << endl;
-		cin.clear();
+	string a;
+	cout << "歡迎，請輸入任何按鈕，Q/q離開" << endl;
+	cin >> a;
+	while (a != "q" && a != "Q") {
+		cout << "請輸入公司名稱" << endl;
+		cin >> ps->name;
+		getline(cin, ps->name);
+		cout << "請輸入PIZZA重量" << endl;
 		cin >> ps->weight;
+		while (cin.fail()) {
+			cout << "請輸入數字" << endl;
+			cin.clear();
+			cin.ignore(256, '\n');
+			cin >> ps->weight;
 
-	}
-	cout << "請輸入PIZZA直徑" << endl;
-	cin >> ps->dim;
-	while (cin.fail()) {
-		cout << "請輸入數字" << endl;
-		cin.clear();
-		cin.ignore(256, '\n');
+		}
+		cout << "請輸入PIZZA直徑" << endl;
 		cin >> ps->dim;
+		while (cin.fail()) {
+			cout << "請輸入數字" << endl;
+			cin.clear();
+			cin.ignore(256, '\n');
+			cin >> ps->dim;
+
+		}
+
+		cout << "公司名稱" << ps->name << endl;
+		cout << "PIZZA直徑" << ps->dim << "公分" << endl;
+		cout << "PIZZA重量" << ps->weight << "公斤" << endl;
+		delete ps;
+
+		cout << "請輸入任何按鈕，Q / q離開" << endl;
+		cin >> a;
 
 	}
-
-	cout << "公司名稱" << ps->name << endl;
-	cout << "PIZZA直徑" << ps->dim << "公分" << endl;
-	cout << "PIZZA重量" << ps->weight << "公斤" << endl;
-	delete ps;
-
-
-
 
 
 
